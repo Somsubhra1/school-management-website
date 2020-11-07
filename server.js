363bcbb7059373f6197089d87b90fa02aa127a95
Somsubhra Das <das.somsubhra1@gmail.com>
2020-11-07 22:42:32 +0530

    auth routes init

--- server.js
+++ server.js
@@ -49,6 +49,8 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.use("/auth", require("./middlewares/auth"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
