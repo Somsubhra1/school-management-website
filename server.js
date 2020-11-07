3412f7c6d192da8057be57f44d1ff18440adbf59
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-11-07 22:33:48 +0530

    added register routes and template

--- server.js
+++ server.js
@@ -31,6 +31,28 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const user = {
+    usertype: req.body.usertype,
+    username: req.body.username,
+    password: req.body.password,
+  };
+  console.log("register:", user);
+});
+
+app.post("/login", (req, res) => {
+  const user = {
+    usertype: req.body.usertype,
+    username: req.body.username,
+    password: req.body.password,
+  };
+  console.log("login:", user);
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
