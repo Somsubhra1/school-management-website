b52b6811fd59f0e994ebb49784ae35dcaed7d4b5
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-11-10 16:03:39 +0530

    integrated yearbook

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,9 @@
 document.getElementById("notice-board").addEventListener("click", () => {
   $("#noticeBoardModal").modal("show");
 });
+
+$(".main-carousel").flickity({
+  // options
+  cellAlign: "left",
+  contain: true,
+});
