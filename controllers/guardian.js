93039cbccd7e5cd9e1849f0040ed9718e0baca60
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-11-18 17:41:07 +0530

    made guardian views dynamic

--- controllers/guardian.js
+++ controllers/guardian.js
@@ -1,5 +1,17 @@
-const showGuardianDashboard = (req, res) => {
-  res.render("guardian/guardian");
+const Student = require("../models/Student");
+
+const showGuardianDashboard = async (req, res) => {
+  const guardianid = req.user._id;
+  // const guardianid = "5faf88b17a91a12e4c81a1eb";
+
+  let students = await Student.find()
+    .populate("guardian", { name: 1, username: 1 }, { _id: guardianid })
+    .exec();
+  students = students.filter((student) => student.guardian != null);
+
+  console.log(students);
+
+  res.render("guardian/guardian", { students });
 };
 
 module.exports = { showGuardianDashboard };
